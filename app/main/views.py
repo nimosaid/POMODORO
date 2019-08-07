6f805af45b6f602dec7ae77b11d280e9f7d12408
nimosaid <saidnimo123@gmail.com>
2019-08-07 18:18:13 +0300

    wrote some code

--- app/main/views.py
+++ app/main/views.py
@@ -6,6 +6,20 @@ from .forms import UpdateProfile
 from .. import db, photos
 
 #Views
+@main.route("/")
+@main.route("/home")
+def index():
+
+  '''
+  View root page function that returns the index page and its data
+  '''
+  title = 'WORK IT!!!!'
+
+  # Getting reviews by category
+  page = request.args.get('page', 1, type=int)
+
+
+  return render_template('index.html')
 @main.route('/user/<uname>/update/pic', methods = ['POST'])
 @login_required
 def profile(uname):
