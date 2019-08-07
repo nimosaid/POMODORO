f4b7128c6c2009bade72eef42daa8b1c7afc8f06
nimosaid <saidnimo123@gmail.com>
2019-08-07 12:10:13 +0300

    wrote some code

--- manage.py
+++ manage.py
@@ -1,16 +1,16 @@
-from app import create_app, db
+from app import create_app
 from flask_script import Manager, Server
-from app.models import User, Pitch, Category, Vote, Comment
+from app.models import User
 from flask_migrate import Migrate, MigrateCommand
 
 # Creating app instance
 app = create_app('production')
 
 manager = Manager(app)
-migrate = Migrate(app, db)
+migrate = Migrate(app)
 
 manager.add_command('server',Server)
-manager.add_command('db', MigrateCommand)
+manager.add_command(MigrateCommand)
 
 manager.add_command('server',Server)
 @manager.command
@@ -23,5 +23,5 @@ def test():
 @manager.shell
 def make_shell_context():
    
-if __name__ == '__main__':
+ if __name__ == '__main__':
     manager.run()
